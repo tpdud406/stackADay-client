63d1527886ea25111811c2d0961baa4d81a6ce28
Areum Han <hanryu1109@gmail.com>
2022-10-17 13:00:50 +0900

    feat: 소켓 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import Main from "./components/Guest";
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
+import Guest from "./components/Guest";
 import Layout from "./components/Layout";
 
 function App() {
+  const [socket, setSocket] = useState();
+  useEffect(() => {
+    const instance = io(`${process.env.SOCKET_SERVER_URL}`);
+    setSocket(instance);
+
+    return () => {
+      instance.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null) return;
+
+    socket.once("load-notices", () => {});
+
+    socket.emit("get-notices");
+  }, []);
+
+  useEffect(() => {
+    if (socket == null) return;
+
+    const interval = setInterval(() => {
+      socket.emit("save-notices");
+    }, 20000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <>
       <Layout />
